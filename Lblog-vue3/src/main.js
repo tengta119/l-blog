c1e3f71528f9a2fab665cebc09145f4e69265286
Bowen Lei <33174642+tengta119@users.noreply.github.com>
2025-11-10 09:46:43 +0800

    feat：前端：路由守卫

--- Lblog-vue3/src/main.js
+++ Lblog-vue3/src/main.js
@@ -2,6 +2,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入全局路由守卫
+import '@/permission'
 
 const app = createApp(App)
 app.use(router)
