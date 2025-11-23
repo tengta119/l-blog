3c91a840542c0d252fc8d47a7af35c78d048a1b0
Bowen Lei <33174642+tengta119@users.noreply.github.com>
2025-11-23 11:18:50 +0800

    前端：feat:文章分类开发

--- Lblog-vue3/src/router/index.js
+++ Lblog-vue3/src/router/index.js
@@ -8,6 +8,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Admin from '@/layouts/admin/admin.vue'
 import ArchiveList from '@/page/frontend/archive-list.vue'
 import CategoryList from '@/page/frontend/category-list.vue'
+import CategoryArticleList from '@/page/frontend/category-article-list.vue'
 // 统一在这里声明所有路由
 const routes = [
     {
@@ -29,6 +30,13 @@ const routes = [
         component: CategoryList,
         meta: { // meta 信息
             title: 'Weblog 分类页'
+        }
+    },
+    {
+        path: '/category/article/list', // 分类文章页
+        component: CategoryArticleList,
+        meta: { // meta 信息
+            title: 'Weblog 分类文章页'
         }
     },
     {
